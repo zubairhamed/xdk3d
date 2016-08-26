8b1238c64587ddf2af99b59a21846cf0ecc26dc1
Zubair Hamed <zubairhamed@gmail.com>
2016-08-26 17:03:43 +0200

    no message

--- run.go
+++ run.go
@@ -3,10 +3,21 @@ package main
 import (
 	"net/http"
 	"log"
+	"github.com/elazarl/go-bindata-assetfs"
 )
 
+const USE_ASSET_FS = true
+
 func main() {
-	fs := http.FileServer(http.Dir("webapp"))
+
+	var fs http.Handler
+	if USE_ASSET_FS  {
+		fs = http.FileServer(&assetfs.AssetFS{Asset: Asset, AssetDir: AssetDir, AssetInfo: AssetInfo, Prefix: "webapp"})
+	} else {
+		fs = http.FileServer(http.Dir("webapp"))
+	}
+
+
 	http.Handle("/", fs)
 	log.Println("Running from :8000. Ready to rock!")
 	http.ListenAndServe(":8000", nil)
@@ -26,5 +37,8 @@ func main() {
 	- If LUX_DATA
 		Calculate Lux
 		Set Lux to all lights
+
+	- If LED Turns on or Off
+		Turn on or off LED
 
  */
