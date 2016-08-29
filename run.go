f37e8f4a78fe08fa0f70d8b128f59c964439bef9
Zubair Hamed <zubairhamed@gmail.com>
2016-08-29 18:58:20 +0200

    3D Updates to blink, temperature etc

--- run.go
+++ run.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-	"net/http"
 	"log"
-	"github.com/elazarl/go-bindata-assetfs"
+	"net/http"
 )
 
 const USE_ASSET_FS = true
 
 func main() {
 
-	var fs http.Handler
-	if USE_ASSET_FS  {
-		fs = http.FileServer(&assetfs.AssetFS{Asset: Asset, AssetDir: AssetDir, AssetInfo: AssetInfo, Prefix: "webapp"})
-	} else {
-		fs = http.FileServer(http.Dir("webapp"))
-	}
+	//var fs http.Handler
+	//if USE_ASSET_FS {
+	//	fs = http.FileServer(&assetfs.AssetFS{Asset: Asset, AssetDir: AssetDir, AssetInfo: AssetInfo, Prefix: "webapp"})
+	//} else {
+	//	fs = http.FileServer(http.Dir("webapp"))
+	//}
 
+	fs := http.FileServer(http.Dir("webapp"))
 
 	http.Handle("/", fs)
 	log.Println("Running from :8000. Ready to rock!")
@@ -24,9 +24,6 @@ func main() {
 }
 
 /*
-	- Render with default lighting
-	- Set rotation to 0
-
 	- If ROT_DATA
 		Set ROT
 
@@ -38,7 +35,4 @@ func main() {
 		Calculate Lux
 		Set Lux to all lights
 
-	- If LED Turns on or Off
-		Turn on or off LED
-
- */
+*/
