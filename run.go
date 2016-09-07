c1c6de41a02a950bc4de5328531cb3d0052639a5
Zubair Hamed <zubairhamed@gmail.com>
2016-09-08 00:42:42 +0200

    icons

--- run.go
+++ run.go
@@ -8,14 +8,6 @@ import (
 const USE_ASSET_FS = true
 
 func main() {
-
-	//var fs http.Handler
-	//if USE_ASSET_FS {
-	//	fs = http.FileServer(&assetfs.AssetFS{Asset: Asset, AssetDir: AssetDir, AssetInfo: AssetInfo, Prefix: "webapp"})
-	//} else {
-	//	fs = http.FileServer(http.Dir("webapp"))
-	//}
-
 	fs := http.FileServer(http.Dir("webapp"))
 
 	http.Handle("/", fs)
